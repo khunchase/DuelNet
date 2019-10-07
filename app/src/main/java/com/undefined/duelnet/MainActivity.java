0930a9865b9cfbf68d905fd809a904a06051564c
chase <chaisun.chang@gmail.com>
2019-10-07 15:44:47 +0900

    Minor changes.

--- app/src/main/java/com/undefined/duelnet/MainActivity.java
+++ app/src/main/java/com/undefined/duelnet/MainActivity.java
@@ -3,6 +3,7 @@ package com.undefined.duelnet;
 import android.os.Bundle;
 import android.webkit.WebView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,3 +17,22 @@ public class MainActivity extends AppCompatActivity {
         myWebView.loadUrl("https://www.duel.so");
     }
 }
+
+FirebaseInstanceId.getInstance().getInstanceId()
+        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+@Override
+public void onComplete(@NonNull Task<InstanceIdResult> task) {
+        if (!task.isSuccessful()) {
+        Log.w(TAG, "getInstanceId failed", task.getException());
+        return;
+        }
+
+        // Get new Instance ID token
+        String token = task.getResult().getToken();
+
+        // Log and toast
+        String msg = getString(R.string.msg_token_fmt, token);
+        Log.d(TAG, msg);
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+        }
+        });
