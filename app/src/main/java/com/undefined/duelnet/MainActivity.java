eb0abb714955c82de941d5588fccdb2745a53ed7
장채선 <chase@chase-mac.local>
2019-10-07 14:01:40 +0900

    Initial commit

---
+++ app/src/main/java/com/undefined/duelnet/MainActivity.java
@@ -0,0 +1,18 @@
+package com.undefined.duelnet;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.duelnet);
+        myWebView.loadUrl("https://www.duel.so");
+    }
+}
