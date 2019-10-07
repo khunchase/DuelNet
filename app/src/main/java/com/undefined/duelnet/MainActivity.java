97765256499649d74a5dfbc27e7b5974124cffb1
chase <chaisun.chang@gmail.com>
2019-10-07 16:19:01 +0900

    Minor changes.

--- app/src/main/java/com/undefined/duelnet/MainActivity.java
+++ app/src/main/java/com/undefined/duelnet/MainActivity.java
@@ -1,13 +1,21 @@
 package com.undefined.duelnet;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
+
+public class MainActivity extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,24 +23,24 @@ public class MainActivity extends AppCompatActivity {
 
         WebView myWebView = (WebView) findViewById(R.id.duelnet);
         myWebView.loadUrl("https://www.duel.so");
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener < InstanceIdResult > () {
+                    @Override
+                    public void onComplete(@NonNull Task < InstanceIdResult > task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("ChaseApp", "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = "" + token;
+                        Log.d("ChaseApp", msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
-
-FirebaseInstanceId.getInstance().getInstanceId()
-        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
-@Override
-public void onComplete(@NonNull Task<InstanceIdResult> task) {
-        if (!task.isSuccessful()) {
-        Log.w(TAG, "getInstanceId failed", task.getException());
-        return;
-        }
-
-        // Get new Instance ID token
-        String token = task.getResult().getToken();
-
-        // Log and toast
-        String msg = getString(R.string.msg_token_fmt, token);
-        Log.d(TAG, msg);
-        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-        }
-        });
